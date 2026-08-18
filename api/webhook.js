bbfe828f6c002ab1949092a5d1189c61ca9980f3
zhmulev <zhmulevav@gmail.com>
2026-08-18 21:55:37 +0700

    Universal proxy

--- api/webhook.js
+++ api/webhook.js
@@ -3,15 +3,18 @@ export default async function handler(req, res) {
     return res.status(405).json({ error: 'Method not allowed' });
   }
 
+  const { path } = req.query;
+  const url = `http://91.132.57.27:8000${path}`;
+
   try {
-    const response = await fetch('http://91.132.57.27:8000/pay/webhook', {
+    const response = await fetch(url, {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify(req.body)
     });
 
     const data = await response.json();
-    return res.status(200).json(data);
+    return res.status(response.status).json(data);
   } catch (error) {
     return res.status(500).json({ error: 'Internal error' });
   }
